96dbd5fb49a383f9d276327dc153595a25019082
yagi <yagi@core-tech.jp>
2012-12-27 16:45:00 +0900

    修正

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 	<title>templete</title>
 	<meta name="keywords" content="">
 	<meta name="description" content="">
-
+	<!--[if lt IE 9]>
+	<script src="../js/html5shiv.js"></script>
+	<![endif]-->
 	<script type="text/javascript" src="../js/jquery.js"></script>
 	<script type="text/javascript" src="../js/base.js"></script>
 	<link type="text/css" rel="stylesheet" href="../css/html5-doctor-reset-stylesheet.css" />
@@ -13,17 +15,18 @@
 
 </head>
 <body>
+
 <div id="wrap">
 
 
 <header>
-	<h1>site-name</h1>
-	<img src="http://cambelt.co/800x200" />
-	<nav>
-		<ul>
-			<li><a href="#">menu1</a></li>
-			<li><a href="#">menu2</a></li>
-			<li><a href="#">menu3</a></li>
+	<h1><a href="#">site-name</a></h1>
+	<p><img src="http://cambelt.co/800x200" /></p>
+	<nav class="floatBox">
+		<ul class="floatBox floatR">
+			<li class="floatL"><a href="#">menu1</a>/</li>
+			<li class="floatL"><a href="#">menu2</a>/</li>
+			<li class="floatL"><a href="#">menu3</a>/</li>
 		</ul>
 	</nav>
 </header>
@@ -31,17 +34,47 @@
 
 <div id="main">
 
-	<div class="w10">
+	<div>
 		<h2>header title(w10)</h2>
 		<p>
 			テキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtext
 		</p>
 	</div>
 
+	<div class="floatBox mt10">
+		<div class="floatL w580 mr10">
+			<h3>header title(w4)</h3>
+			<p>
+				テキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtextテキストtext
+			</p>
+		</div>
+
+		<div class="floatL w200">
+			<div>
+				<h3>about</h3>
+				<dl>
+				<dt><img src="http://cambelt.co/200x40"></dt>
+				<dd>http://XXXXXXX.XXXX</dd>
+				</dl>
+			</div>
+			<div class="mt10">
+				<h3>header title(w6)</h3>
+				<nav>
+					<ul>
+						<li><img src="http://cambelt.co/200x40"></li>
+						<li><img src="http://cambelt.co/200x40"></li>
+						<li><img src="http://cambelt.co/200x40"></li>
+						<li><img src="http://cambelt.co/200x40"></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+
 </div>
 
 <footer>
-	copyright&reg;site-name 2009-<?php echo date('Y')?>
+	<p>copyright&reg;site-name 2009-<?php echo date('Y')?><p>
 </footer>
 </div>
 </body>
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 ﻿#Html templete
 
 ## 概要
-*※2012/12現在*
-ポートフォリオサイト用htmlテンプレート
-作成@hajime0083
+*※2012/12現在未完成*
+
+自サイト用htmlテンプレート
+作成@[hajime0083](https://twitter.com/hajime0083)
+公開アドレス：未定
 
 ## 作成
 * html5
@@ -14,9 +16,12 @@
 ### html
 スタート時テンプレート作成
 [HTML5 TEMPKATE GENERATOR](http://mizzz.jp/tg/)
+
+IE対策用js
+[html5shiv](https://github.com/aFarkas/html5shiv)
 ###css
 CSSリセット
-[HTML5 Doctor CSS Reset](http://www.cssreset.com/scripts/html5-doctor-css-reset-stylesheet/)  v1.6.1(2012/12現在)
+[HTML5 Doctor CSS Reset](http://www.cssreset.com/scripts/html5-doctor-css-reset-stylesheet/) 
 ###js
 jquery
-[jquery](http://jquery.com/) v1.8.3(2012/12現在)
+[jquery](http://jquery.com/)
